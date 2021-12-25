2c75d1a193b7de35eb3273dca9692e84074e1a60
Denis Krüger <deniskrueger03@gmail.com>
2021-12-25 19:40:11 +0100

    Removed and refactored testing.py

--- testing.py
+++ testing.py
@@ -7,22 +7,9 @@ from helper import constants
 import numpy as np
 
 
-def imgio():
-    inpt = imageio.read_image("test_cases/example_easy.jpg")
-    print(inpt)
-    for x in inpt:
-        if len(x) != 9:
-            print("wrong")
-            exit(0)
+def test_ai(file):
+    inpt = imageio.read_image(f"test_cases/{file}.jpg")
             
     s = sudoku.Sudoku(inpt)
-    slvd = s.solve()
-    # imageio.save(inpt, slvd, "solved.jpg")
 
-
-test = np.array(constants.TEST_SUDOKU)
-
-imgio()
-
-# for t, p in zip(test.flatten(), predict):
-#     print(f"test: {t}  |   predict: {p}")
+test_ai("example_easy")
